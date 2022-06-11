1de78cdd07c6702450efc3d719401e1674655f64
Elliott Polite <elliottp@iMacs-iMac.local>
2022-06-11 12:09:51 -0600

    first commit

---
+++ BankEncapsulationExercise/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BankEncapsulationExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
